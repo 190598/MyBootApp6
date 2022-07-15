54948ee5b98f962043adbe6fdf0c9df9038f22c3
190598 <te190598@gmail.com>
2022-07-15 18:43:05 +0900

    kadai7 re

--- src/main/java/jp/te4a/spring/boot/myapp7/MyBootApp7Application.java
+++ src/main/java/jp/te4a/spring/boot/myapp7/MyBootApp7Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyBootApp7Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MyBootApp6Application.class, args);
+		SpringApplication.run(MyBootApp7Application.class, args);
 	}
 
 }
